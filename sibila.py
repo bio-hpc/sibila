86f250c29276c5f55fc1e2fc92bb05356e5ff84b
Antonio Jesús Banegas Luna <ajbanegas@ucam.edu>
2022-11-09 10:03:00 +0100

    Times plot is always displayed, even when interpretability is skipped

--- sibila.py
+++ sibila.py
@@ -99,6 +99,8 @@ def execute(x, y, id_list, idx_samples, io_data, folder_experiment, file_dataset
     if not args.skip_interpretability:
         Interpretability(Serialize(model.get_model(), xtr, ytr, xts, yts, id_list, cfg, io_data, idx_xts))
 
+    Graphics().plot_times(cfg.get_folder(), cfg.get_prefix(), type_model)
+
 
 def execute_pred(x, y, id_list, idx_samples, io_data, folder_experiment, file_dataset, type_model, args):
     cfg = get_basic_cfg(folder_experiment, file_dataset, args)
