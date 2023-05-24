29af34d48fd70cbbc447c66c3c3fdee8b94f6554
bio-hpc <94918133+bio-hpc@users.noreply.github.com>
2023-05-24 11:26:52 +0200

    Update LimeRuleExtractor.py

--- Tools/PostProcessing/LimeRuleExtractor.py
+++ Tools/PostProcessing/LimeRuleExtractor.py
@@ -18,7 +18,7 @@ from os.path import isdir, join
 class LimeRuleExtractor():
     def __init__(self, folder):
         self.folder = join(getcwd(), folder)
-        self.pattern = '*_lime_explain_*'
+        self.pattern = '*_Lime_explain_*'
 
     def extract_rule(self):
         # list all local interpretations in directory
@@ -31,7 +31,7 @@ class LimeRuleExtractor():
         for df in dfs:
             df_aux = df.groupby(['range','class']).agg(
                 count_col=pd.NamedAgg(column='range', aggfunc='count'),
-                weigth_mean=pd.NamedAgg(column='weight', aggfunc='mean')
+                weigth_mean=pd.NamedAgg(column='attribution', aggfunc='mean')
             )
             df_aux['coeff'] = df_aux['count_col']*df_aux['weigth_mean']
             target_id = df_aux.index[0][1]
