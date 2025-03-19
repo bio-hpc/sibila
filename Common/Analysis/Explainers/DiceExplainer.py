8f2d7be8e7fd4c04247d5e23cbd12dd364e185e8
Antonio Jesús Banegas Luna <ajbanegas@ucam.edu>
2025-03-19 22:37:55 +0100

    Revert "Updated configuration of counterfactual explanations"
    This reverts commit f36d80ae6e4244d835a957eb916d408dd643bc3c.

--- Common/Analysis/Explainers/DiceExplainer.py
+++ Common/Analysis/Explainers/DiceExplainer.py
@@ -9,7 +9,7 @@ from Common.Analysis.Explainers.ExplainerModel import ExplainerModel
 from Tools.ToolsModels import is_tf_model, is_ripper_model, is_rulefit_model, is_regression_by_config, is_multiclass
 from Tools.Estimators.RipperEstimator import RipperEstimator
 from pathlib import Path
-from Common.Config.ConfigHolder import FEATURE, ATTR, STD, COLNAMES, PROBA, TRUEVAL, PREDVAL
+from Common.Config.ConfigHolder import FEATURE, ATTR, STD, COLNAMES, PROBA
 from Tools.ToolsModels import is_regression_by_config
 
 class DiceExplainer(ExplainerModel):
@@ -17,30 +17,26 @@ class DiceExplainer(ExplainerModel):
     DICE_METHOD = "random"  # random | genetic | kdtree
 
     def explain(self):
-        if not is_ripper_model(self.model) and not is_rulefit_model(self.model):
+        if not is_tf_model(self.model) and not is_ripper_model(self.model) and not is_rulefit_model(self.model):
             return self.execute_dice()
 
     def execute_dice(self):
         df = pd.DataFrame(self.xts, columns=self.id_list)
+
         df['class'] = self.yts
         lst_features = self.id_list
-
         d = dice_ml.Data(dataframe=df, continuous_features=lst_features, outcome_name='class')
-        backend = 'TF2' if is_tf_model(self.model) else 'sklearn'
-        feature_range = (self.xtr.min(axis=0) - 0.1, self.xtr.max(axis=0) + 0.1)
+        backend = "TF2" if is_tf_model(self.model) else 'sklearn'
 
         model_type = 'classifier'
         desired_range = None
-        sparsity = 'binary'
         if is_regression_by_config(self.cfg):
             model_type = 'regressor'
             desired_range = (min(self.ytr), max(self.ytr))
-            sparsity = 'linear'
 
         m = dice_ml.Model(model=self.model, backend=backend, model_type=model_type)
         exp = dice_ml.Dice(d, m, method=self.DICE_METHOD)
-        #exp.update_hyperparameters(feature_range=feature_range)    
-        
+            
         self.df_local = [None] * len(self.xts)
         desired_class = 0 if is_multiclass(self.cfg) else "opposite"
 
@@ -51,19 +47,10 @@ class DiceExplainer(ExplainerModel):
                     total_CFs = 10,
                     desired_class = desired_class,
                     features_to_vary = lst_features,
-                    posthoc_sparsity_algorithm = sparsity,
+                    posthoc_sparsity_algorithm = "binary",
                     random_seed = self.cfg.get_args()['seed'],
-                    desired_range = desired_range,
-                    proximity_weight = 0.01,
-                    diversity_weight = 0.01,
-                    permitted_range = None
-                    #feature_range = feature_range
-                    #learning_rate = 0.1
+                    desired_range = desired_range
             )
-
-            if e1.cf_examples_list[0].final_cfs_df is not None:
-                print('No counterfactuals were found')
-                pass
 
             lst = [e1] if is_tf_model(self.model) else e1.cf_examples_list
             for i in tqdm(range(len(self.xts))):
@@ -75,14 +62,9 @@ class DiceExplainer(ExplainerModel):
                       )
                 self.df_local[i] = pd.DataFrame(imp.local_importance).mean(axis=0).to_frame().reset_index()
                 self.df_local[i].columns = [FEATURE, ATTR]
-                if is_regression_by_config(self.cfg):
-                    self.df_local[i][TRUEVAL] = self.yts[i]
-                    self.df_local[i][PREDVAL] = self.proba_sample(self.xts[i])
-                else:
-                    self.df_local[i][PROBA] = self.proba_sample(self.xts[i])
+                self.df_local[i][PROBA] = self.proba_sample(self.xts[i])
         except BaseException as e:
-            #print(str(e))
-            pass
+            print(str(e))
         finally:
             if 'e1' in locals():
                 del e1
@@ -115,7 +97,6 @@ class DiceExplainer(ExplainerModel):
 
             # Sort in ascending order for plotting correctly
             df2 = self.df_local[i]
-
             self.io_data.save_dataframe_cols(df2, df2.columns, path_csv)
 
             # Add the real value into the label
@@ -130,5 +111,4 @@ class DiceExplainer(ExplainerModel):
     def get_value(self, feature, row_id):
         index = self.id_list.index(feature)
         return self.xts[row_id, index]
-
 
