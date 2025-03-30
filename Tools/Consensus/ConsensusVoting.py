b26705a797e342e61535b88b47f747401d9306dc
Antonio Jesús Banegas Luna <ajbanegas@ucam.edu>
2025-03-30 11:54:12 +0200

    corrected voting and wscf

--- Tools/Consensus/ConsensusVoting.py
+++ Tools/Consensus/ConsensusVoting.py
@@ -14,20 +14,31 @@ class ConsensusVoting(ConsensusBase):
     def __init__(self, folder, dir_out):
         super(ConsensusVoting, self).__init__(folder, dir_out)
         self.title = 'Voting'
+        self.N = 5
 
     def consensus(self):
         print("Computing feature voting")
+      
+        # global methods
+        dfg = self.df_g[self.df_g[self.RANKING] <= self.N]
+        dfg_cnt = dfg[self.FEATURE].value_counts().to_frame().reset_index()
+        dfg_cnt.columns = [self.FEATURE, self.ATTR]
 
-        df = pd.concat([self.df_g, self.df_l], ignore_index=True)
-        df = df[[self.FEATURE, self.ATTR, self.RANKING]]
+        # local methods
+        dfl = self.df_l[self.df_l[self.RANKING] <= self.N]
+        dfl_cnt = dfl[self.FEATURE].value_counts().to_frame().reset_index()
+        dfl_cnt.columns = [self.FEATURE, self.ATTR]
+        dfl_cnt[self.ATTR] = dfl_cnt[self.ATTR] / self.df_l.shape[0] # total count rated by the number of samples
 
+        # add the counters of local and global methods
+        df = pd.concat([dfg_cnt, dfl_cnt], ignore_index=True)
+        df_mean = df.groupby(self.FEATURE, as_index=False)[self.ATTR].sum()
+
+        #df = pd.concat([self.df_g, self.df_l], ignore_index=True)
+        #df = df[[self.FEATURE, self.ATTR, self.RANKING]]
         # voting features
-        df = df[df[self.RANKING] <= 10]
-        df_mean = df[self.FEATURE].value_counts().to_frame().reset_index()
-        df_mean.columns = [self.FEATURE, self.ATTR]
+        #df = df[df[self.RANKING] <= 10]
+        #df_mean = df[self.FEATURE].value_counts().to_frame().reset_index()
+        #df_mean.columns = [self.FEATURE, self.ATTR]
 
-        # output
-        #features = df_mean[self.FEATURE].to_numpy()
-        #attrs = df_mean[self.ATTR].to_numpy()
-        #return features, attrs
         return df_mean
--- Tools/Consensus/ConsensusWSCF.py
+++ Tools/Consensus/ConsensusWSCF.py
@@ -18,15 +18,6 @@ WSCF: Weighted and Scaled Consensus Function
 """
 class ConsensusWSCF(ConsensusBase):
 
-    #WEIGHTS = {
-    #    "Shapley/csv": 0.350,
-    #    "RFPermutationImportance": 0.250,
-    #    "PermutationImportance": 0.190,
-    #    "LIME": 0.190,
-    #    "IntegratedGradients/csv": 0.010,
-    #    "Dice": 0.010
-    #}
-
     def __init__(self, folder, dir_out):
         super(ConsensusWSCF, self).__init__(folder, dir_out)
         self.title = 'WSCF'
@@ -34,11 +25,15 @@ class ConsensusWSCF(ConsensusBase):
     def consensus(self):
         print("Computing WSCF")
 
+        #if not self.is_regression:
+        #    alfa = (4*pow(self.model_acc[0], 2))-(4*self.model_acc[0])+1 # model_acc = AUC
+        #else:
+        #    alfa = self.model_acc[0] # model_acc = R2
+
         # a) global methods
         # reload the data to manipulate it individually
         models, prefixes = super().find_models()
         self.df_g = None
-        scaler = MinMaxScaler(feature_range=(0, 1))
 
         for idx in range(len(models)):
             for g in self.GLOBALS:
@@ -47,9 +42,8 @@ class ConsensusWSCF(ConsensusBase):
                     continue
                 df = super().load_csv(foo)
 
-                # scale in range [0,1]
-                df[self.ATTR] = scaler.fit_transform(df[[self.ATTR]])
-                #df[self.ATTR] *= self.WEIGHTS[g] # TODO
+                # scale in range [0,1] while keeping the sign
+                df = self._scale(df)
                 self.df_g = super().append_df(self.df_g, df)
 
         self.df_g['std'] = self.df_g['std'].fillna(0)
@@ -57,7 +51,6 @@ class ConsensusWSCF(ConsensusBase):
         # b) local methods
         # reload the data to manipulate individually
         self.df_l = None
-        scaler = MinMaxScaler(feature_range=(0, 1))
 
         for idx in range(len(models)):
             for l in self.LOCALS:
@@ -70,13 +63,12 @@ class ConsensusWSCF(ConsensusBase):
                 for foo in foos:
                     df = self.load_csv(foo)
 
-                    # scale in range [0,1]
-                    df[self.ATTR] = scaler.fit_transform(df[[self.ATTR]])
+                    # scale in range [0,1] while keeping the sign
+                    df = self._scale(df)
 
                     # divide by the number of samples to avoid bias against global methods
                     N = df.shape[0]
                     df[self.ATTR] = df[[self.ATTR]] / N
-                    #df[self.ATTR] *= self.WEIGHTS[l] # TODO
                     self.df_l = self.append_df(self.df_l, df)
 
         # apply correction factor to local methods for classification and regression
@@ -85,7 +77,7 @@ class ConsensusWSCF(ConsensusBase):
             self.df_l['exp_factor'] = self.df_l['error'].apply(lambda x: self._exponential_factor(x, alpha=0.5)/N)
             self.df_l[self.ATTR] = self.df_l[self.ATTR] * self.df_l['exp_factor']           
         else:
-            self.df_l[self.ATTR] *= (4 * (self.df_l[self.PROBA]**2 - self.df_l[self.PROBA]) + 1)
+            self.df_l[self.ATTR] *= (4 * (pow(self.df_l[self.PROBA], 2) - self.df_l[self.PROBA]) + 1)
 
         # merge both transformed explanations
         df = pd.concat([self.df_g, self.df_l], ignore_index=True)
@@ -99,3 +91,21 @@ class ConsensusWSCF(ConsensusBase):
 
     def _sigmoid_factor(self, error, beta=0.5):
         return 1 / (1 + beta * abs(error))
+
+    def _scale(self, df):
+        # split positive and negative attributions
+        pos = df[self.ATTR] > 0
+        neg = df[self.ATTR] < 0
+
+        # create a separate scale for each set
+        scaler_pos = MinMaxScaler(feature_range=(0, 1))
+        scaler_neg = MinMaxScaler(feature_range=(-1, 0))
+
+        # scale in range (-1,0) or (0,1) depending on the values
+        if pos.sum() > 0:
+            df.loc[pos, self.ATTR] = scaler_pos.fit_transform(df.loc[pos, [self.ATTR]])
+        if neg.sum() > 0:
+            df.loc[neg, self.ATTR] = scaler_neg.fit_transform(df.loc[neg, [self.ATTR]])
+
+        return df
+
